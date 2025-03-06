97682b5bdf5d9973db96b0c7a0b2a56913397e44
j.colaco <j.colaco@tapoly.com>
2025-03-06 00:42:29 +0000

    basic minimally styled chain reaction game

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import ChainReactionGame from "./components/ChainReactionGame";
+import word_list from "./word_list";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [wordList, setWordList] = useState<string[]>([]);
+  const [inputWords, setInputWords] = useState<number[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    if (wordList == null || wordList.length === 0) {
+      const randomWordList =
+        word_list[Math.floor(Math.random() * word_list.length)];
+
+      setWordList(randomWordList);
+    }
+  }, []);
+
+  return <ChainReactionGame words={wordList} />;
 }
 
-export default App
+export default App;
